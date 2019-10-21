ad71398c07942668eb8700fdfb8cc659a9f8877f
Rifa Safeer Shah <rifashah28@gmail.com>
2019-10-21 00:05:22 -0700

    Create hello_name.py

---
+++ Python/String-1/hello_name.py
@@ -0,0 +1,19 @@
+#Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+#hello_name('Bob') → 'Hello Bob!'
+#hello_name('Alice') → 'Hello Alice!'
+#hello_name('X') → 'Hello X!'
+
+def hello_name(name):
+  return "Hello " + name + "!"
+  
+#hello_name('Bob') → 'Hello Bob!'
+#hello_name('Alice') → 'Hello Alice!'
+#hello_name('X') → 'Hello X!'
+#hello_name('Dolly') → 'Hello Dolly!'
+#hello_name('Alpha') → 'Hello Alpha!'
+#hello_name('Omega') → 'Hello Omega!'
+#hello_name('Goodbye') → 'Hello Goodbye!'
+#hello_name('ho ho ho') → 'Hello ho ho ho!'
+#hello_name('xyz!') → 'Hello xyz!!'
+#hello_name('Hello') → 'Hello Hello!'
