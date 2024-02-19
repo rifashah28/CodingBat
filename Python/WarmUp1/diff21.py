722e22eeb0bfba8e4e881597d8bc85a9a0f6ab2f
Rifa Safeer Shah <rifa.safeershah@gmail.com>
2024-02-19 13:22:39 -0800

    Update diff21.py

--- Python/WarmUp1/diff21.py
+++ Python/WarmUp1/diff21.py
@@ -9,16 +9,29 @@ def diff21(n):
     return abs(n-21)
   else:
     return 2 * abs(n-21)
-    
-#diff21(19) → 2
-#diff21(10) → 11
-#diff21(21) → 0
-#diff21(22) → 2
-#diff21(25) → 8
-#diff21(30) → 18
-#diff21(0) → 21
-#diff21(1) → 20
-#diff21(2) → 19
-#diff21(-1) → 22
-#diff21(-2) → 23
-#diff21(50) → 58
+
+'''
+def diff21(n):
+  if n > 21:
+    return (21 - n) * -2
+  else:
+    return 21 - n
+
+def diff21(n):
+  if n <= 21:
+    return 21 - n
+  else:
+    return (n - 21) * 2
+'''
+#diff21(19) → 2    OK
+#diff21(10) → 11   OK
+#diff21(21) → 0    OK
+#diff21(22) → 2    OK
+#diff21(25) → 8    OK
+#diff21(30) → 18   OK
+#diff21(0) → 21    OK
+#diff21(1) → 20    OK
+#diff21(2) → 19    OK
+#diff21(-1) → 22   OK
+#diff21(-2) → 23   OK
+#diff21(50) → 58   OK
